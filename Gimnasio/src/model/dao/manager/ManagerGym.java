5cf5716f3f6e3eae30fea79308fd1d10490b17b1
juank <juank20097@gmail.com>
2014-08-28 00:24:09 -0500

    -Adicion de Metodos ManagerGym

--- Gimnasio/src/model/dao/manager/ManagerGym.java
+++ Gimnasio/src/model/dao/manager/ManagerGym.java
@@ -1,5 +1,7 @@
 package model.dao.manager;
 
+import java.math.BigDecimal;
+import java.util.Date;
 import java.util.List;
 
 import javax.persistence.EntityManager;
@@ -13,6 +15,7 @@ import model.dao.entities.Tipousr;
 public class ManagerGym {
 	private static EntityManagerFactory emf;
 	private static EntityManager em;
+	private static Tipousr tipo;
 
 	
 	
@@ -23,18 +26,113 @@ public ManagerGym() {
 			em=emf.createEntityManager();
 	}
 
-	//USUARIOS
-
-	//Listar Todos los Usuarios
-	public List<Usuario> findAllUsuarios(){
-	 List<Usuario> listado;
-	 Query q;
-	 em.getTransaction().begin();
-	 q=em.createQuery("SELECT u FROM Usuario u ORDER BY u.idUsr");
-	 listado= q.getResultList();
-	 em.getTransaction().commit();
-	 return listado;
-	 
-	}
 	
+	//TIPOS DE USUARIO
+
+		//Listar Todos los tiposUsr
+		public List<Tipousr> findAllTipos(){
+		 List<Tipousr> listado;
+		 Query q;
+		 em.getTransaction().begin();
+		 q=em.createQuery("SELECT u FROM Tipousr u ORDER BY u.idTipousr");
+		 listado= q.getResultList();
+		 em.getTransaction().commit();
+		 return listado;
+		 
+		}
+		
+		//metodo ingresar tipo
+		 public void crearTipo (Long idTipoUsr,String tipo, String descripcion){
+			 em.getTransaction().begin();
+			 Tipousr u= new Tipousr();
+			 u.setIdTipousr(idTipoUsr);
+			 u.setDescripcion(descripcion);
+			 u.setTipo(tipo);
+			 em.persist(u);
+			 em.getTransaction().commit();
+			 
+		 }
+		 
+		 //metodo para buscar un tipo por id
+		 	public Tipousr findbyIdTipo(Long idTipoUsr){
+			 em.getTransaction().begin();
+			 Tipousr t= em.find(Tipousr.class, idTipoUsr);
+			 em.getTransaction().commit();
+			 return t;
+		 }
+		 	
+		 //metodo eliminar tipo
+		 	public void eliminarTipo(Long idTipoUsr){
+		 		//buscamos el tipo a ser eliminado
+		 		Tipousr t=findbyIdTipo(idTipoUsr);
+		 		em.getTransaction().begin();
+		 		em.remove(t);
+		 		em.getTransaction().commit();
+		 	}
+		 	
+		 	
+		 	//USUARIOS
+
+			//Listar Todos los Usuarios
+			public List<Usuario> findAllUsuarios(){
+			 List<Usuario> listado;
+			 Query q;
+			 em.getTransaction().begin();
+			 q=em.createQuery("SELECT u FROM Usuario u ORDER BY u.idUsr");
+			 listado= q.getResultList();
+			 em.getTransaction().commit();
+			 return listado;
+			 
+			}
+			
+			
+			//metodo ingresar usuario
+			 public void crearUsuario (Long idUsr,String nick,String nombres,String pass){
+				 em.getTransaction().begin();
+				 Usuario u= new Usuario();
+				 u.setIdUsr(idUsr);
+				 u.setNick(nick);
+				 u.setNombres(nombres);
+				 u.setPass(pass);
+				 u.setTipousr(tipo);
+				 em.persist(u);
+				 em.getTransaction().commit();
+				 
+			 }
+			 
+			 //metodo para buscar usuario por id
+			 	public Usuario findbyIdUsuario(Long idUsr){
+				 em.getTransaction().begin();
+				 Usuario u= em.find(Usuario.class, idUsr);
+				 em.getTransaction().commit();
+				 return u;
+			 }
+			 	
+			 //metodo eliminar usuario
+			 	public void eliminarUsuario(Long idUsr){
+			 		//buscamos usuario a ser eliminado
+			 		Usuario u=findbyIdUsuario(idUsr);
+			 		em.getTransaction().begin();
+			 		em.remove(u);
+			 		em.getTransaction().commit();
+			 	}
+
+			 	public Tipousr asignarTipoUsuario(Long idTipoUsr) {
+			 		tipo=findbyIdTipo(idTipoUsr);
+			 		return tipo;
+				}
+			 	
+			 	//metodo para actualizar usuario
+			 	 public void actualizarHora(Long idUsr,String nick,String nombres,String pass){
+					 //buscamos el objeto que debe ser actualizado:
+					 Usuario u = findbyIdUsuario(idUsr);
+					 em.getTransaction().begin();
+					 // no se actualiza la clave primaria, en este caso solo la descripcion
+					 u.setNick(nick);
+					 u.setNombres(nombres);
+					 u.setPass(pass);
+					 em.merge(u);
+					 em.getTransaction().commit();
+				 }		 	
+		 	
 }
