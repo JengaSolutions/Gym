70d42a9ec6ea74e7ba87ad85ae72eef6de1cbd21
juank <juank20097@gmail.com>
2014-08-28 12:30:00 -0500

    -adicion de metodos a tipoUsuario

--- Gimnasio/src/model/dao/controller/TipoUsrBean.java
+++ Gimnasio/src/model/dao/controller/TipoUsrBean.java
@@ -1,5 +1,74 @@
 package model.dao.controller;
 
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.SessionScoped;
+
+import model.dao.entities.Tipousr;
+import model.dao.manager.ManagerGym;
+
+
+@SessionScoped
+@ManagedBean
 public class TipoUsrBean {
 
+private ManagerGym manager;
+
+private Long idTipousr;
+
+private String descripcion;
+
+private String tipo;
+
+public TipoUsrBean (){
+	manager=new ManagerGym();
 }
+
+public Long getIdTipousr() {
+	return idTipousr;
+}
+
+public void setIdTipousr(Long idTipousr) {
+	this.idTipousr = idTipousr;
+}
+
+public String getDescripcion() {
+	return descripcion;
+}
+
+public void setDescripcion(String descripcion) {
+	this.descripcion = descripcion;
+}
+
+public String getTipo() {
+	return tipo;
+}
+
+public void setTipo(String tipo) {
+	this.tipo = tipo;
+}
+
+//accion para invocar el manager y crear tipo
+	public String actionCrearTipo(){
+		manager.crearTipo(idTipousr, tipo, descripcion);
+		//reiniciamos datos (limpiamos el formulario)
+		idTipousr=null;
+		tipo="";
+		descripcion="";
+		return "";
+	}
+	
+//accion para eliminar un tipo
+	public String EliminarTipo(Tipousr tipousr){
+		manager.eliminarTipo(tipousr.getIdTipousr());
+		return "";
+	}
+	
+//redireccion a Usuario
+	public String irUsuario(){
+		return "usuario";
+	}
+
+}
+
+
+
